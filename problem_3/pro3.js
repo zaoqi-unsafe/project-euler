019e98a7bb66362003282768c5ac41f8cad0ef29
Adrian Statescu <mergesortv@gmail.com>
2013-04-16 23:17:45 -0700

    problem solved in js

--- problem_3/pro3.js
+++ problem_3/pro3.js
@@ -9,3 +9,32 @@
 
    What is the largest prime factor of the number 600851475143 ? 
 */
+
+function factorsPrimes( n ) {
+
+         var i = 2;
+
+         var out = ""
+
+         var f
+         
+         do {
+
+            f = 0
+
+            while(n % i == 0) {
+
+                 f++;
+
+                 n = parseInt(n/i)
+            }                 
+            if(f) out += i + "^" + f + " * "
+
+            i++;
+
+         }while(!(n==1));
+
+    return out;
+}
+
+console.log(factorsPrimes(600851475143))
