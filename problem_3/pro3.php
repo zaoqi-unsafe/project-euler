f21d83db5b241d51c52292d8af4c91152641d11a
Adrian Statescu <mergesortv@gmail.com>
2013-04-18 23:47:09 -0700

    pro3 in php

--- problem_3/pro3.php
+++ problem_3/pro3.php
@@ -1,3 +1,4 @@
+<?php
 /** 
    Project Euler
 
@@ -9,3 +10,63 @@
 
    What is the largest prime factor of the number 600851475143 ? 
 */
+
+function largestFactorPrime( $n ) {
+
+         $i = 2;
+
+         $out = "";
+
+         $largest = 2;
+
+         $fm = 0;
+
+         do {
+
+            $fm = 0;
+
+            while( ($n % $i) == 0 ) {
+
+                 $fm++;
+
+                 $n = intval($n / $i); 
+            }
+
+           if($fm) $out .= $i . "^". $fm . "*";
+
+           $largest = $i;
+
+           $i++;
+
+         } while($n != 1) ;
+
+   return array("factors" => $out, "largest" => ($i-1)); 
+}
+
+function factores($n) {
+
+    $p = 2;
+$factores = array();
+
+while ($p * $p <= $n) {
+if (fmod($n, $p) == 0) {
+$factores[] = $p;
+$n = $n / $p;
+} else {
+$p++;
+}
+}
+
+if ($n != 1) {
+$factores[] = $n;
+}
+
+return $factores;
+}
+//$out = largestFactorPrime( 600851475143 );
+
+//echo $out["factors"];
+echo"<pre>";
+print_r( factores(1001) );
+echo"</pre>";
+?>
