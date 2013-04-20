937e39332b83c35c7094bbe02d113c47fe36a036
Adrian Statescu <mergesortv@gmail.com>
2013-04-19 23:30:53 -0700

    solved in python with smallest algorithm

--- problem_3/pro3.py
+++ problem_3/pro3.py
@@ -3,3 +3,27 @@
 # Problem 3
 # The prime factors of 13195 are 5, 7, 13, and 29.
 # What is the largest prime factor of the number 600851475143 ? 
+
+def smallest_factor(n):
+    i = 2
+    while i < n:
+        if 0 == n % i:
+           return n
+        i = i + 1
+    return i
+
+def largest_prime_factor(n):
+
+    m = n
+    l = 1
+  
+    while m != 1:
+          if m <= 2:
+             return m
+          else:
+             j = smallest_factor(m)
+             m = m / j
+             l = j
+    return l
+
+print largest_prime_factor(10)
